d2cd870bc5f6ccf2fbe146d321fc8aabbfac02f2
gabrielppd77 <gabrielppd77@outlook.com>
2023-04-25 12:20:15 -0300

    start all configurations bases

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,20 @@
+import { colors } from '@cherry-bomb-ui/tokens'
+
+export function App() {
+  return (
+    <h1
+      style={{
+        color: colors.red,
+      }}
+    >
+      <h2
+        style={{
+          color: colors.blue,
+        }}
+      >
+        Here
+      </h2>
+      TS1
+    </h1>
+  )
+}
